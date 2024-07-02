8c7d83f88c89319a4d38be9cd2aa84bcb184b341
jmore3 <84981020+jmore3@users.noreply.github.com>
2024-07-02 02:01:49 -0600

    Update led.c
    initially was lost, but realized demo 10 had what i needed.

--- demos/button_demo/led.c
+++ demos/button_demo/led.c
@@ -10,7 +10,14 @@ void led_init()
 }
 
 void led_update(){
-    if (switch_state_changed) {
-        //do something
-    }
+  if (switch_state_changed) {
+    char ledFlags = 0; /* by default, no LEDs on */
+
+    ledFlags |= switch_state_down ? LED_GREEN : 0;
+    ledFlags |= switch_state_down ? 0 : LED_RED;
+
+    P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
+    P1OUT |= ledFlags;         // set bits for on leds
+  }
+  switch_state_changed = 0;
 }
