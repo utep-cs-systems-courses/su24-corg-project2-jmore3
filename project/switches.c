f1c13861ae29d454a792d72f321694a419f4ea67
jmore3 <84981020+jmore3@users.noreply.github.com>
2024-07-02 02:45:59 -0600

    Update switches.c

--- project/switches.c
+++ project/switches.c
@@ -2,6 +2,7 @@
 #include "switches.h"
 #include "leds.h"
 #include "libTimer.h"
+#include "melody.h"
 
 char switch_state_downSW1,switch_state_downSW2, switch_state_downSW3, switch_state_downSW4, switch_state_changed;
 
@@ -50,4 +51,43 @@ switch_init()
   P2DIR &= ~SWITCHES4;
   switch_update_interrupt_sense();
   led_update();
+}
+
+void
+switch_interrupt_handlerSW1()
+{
+  char p2val = switch_update_interrupt_sense();
+  switch_state_downSW1 = (p2val & SW1) ? 0 : 1; //if SW1 is up, then is 0
+  switch_state_changed = 1;
+  led_dim_update();
+  
+}
+void
+switch_interrupt_handlerSW2()
+{
+  char p2val = switch_update_interrupt_sense();
+  switch_state_downSW2 = (p2val & SW2) ? 0 : 1; //if SW2 is up, then is 0
+  switch_state_changed = 1;
+  led_update();
+  
+}
+void
+switch_interrupt_handlerSW3()
+{
+  char p2val = switch_update_interrupt_sense();
+  switch_state_downSW3 = (p2val & SW3) ? 0 : 1; //if SW3 is up, then is 0
+  switch_state_changed = 1;
+  led_update_green();
+  melody_play();
+  
+}
+void
+switch_interrupt_handlerSW4()
+{
+  char p2val = switch_update_interrupt_sense();
+  switch_state_downSW4 = (p2val & SW4) ? 0 : 1;
+  switch_state_changed = 1;
+  //led_siren();
+  audio_siren();
+  
 }
