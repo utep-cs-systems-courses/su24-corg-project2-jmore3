5281d673af70abb1e7c4630645e05e35148a3e86
jmore3 <84981020+jmore3@users.noreply.github.com>
2024-07-02 01:54:17 -0600

    Create led.c

---
+++ demos/button_demo/led.c
@@ -0,0 +1,16 @@
+#include <msp430.h>
+#include "led.h"
+#include "switches.h"
+
+void led_init()
+{
+  P1DIR |= LEDS;		// bits attached to leds are output
+  switch_state_changed = 1;
+  led_update();
+}
+
+void led_update(){
+    if (switch_state_changed) {
+        //do something
+    }
+}
