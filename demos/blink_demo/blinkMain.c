82b99d46c481b1f0d4a7c5e9c8f88fbb53cd2f83
jmore3 <84981020+jmore3@users.noreply.github.com>
2024-07-02 01:38:15 -0600

    setup Main
    Main now set up to configure clocks, and enable periodic interrupts.

--- demos/blink_demo/blinkMain.c
+++ demos/blink_demo/blinkMain.c
@@ -2,5 +2,9 @@
 #include <msp430.h>
 
 int main(void) {
-  
+  configureClocks(); /* setup master oscillator, CPU & peripheral clocks */
+  led_init();
+  enableWDTInterrupts(); /* enable periodic interrupt */
+
+  or_sr(0x18); /* CPU off, GIE on */
 }
